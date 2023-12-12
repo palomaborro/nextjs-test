a61b90471c73a21163a405c4782e5caa0ec1163a
palomaborro <palomabs93@gmail.com>
2023-12-12 10:18:43 +0100

    Create nextjs dashboard

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -5,7 +5,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <h1>Este título</h1>
+        <div style={{ border: "1px solid #000" }}>{children}</div>
+      </body>
     </html>
   );
 }
